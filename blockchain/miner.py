5683010dc64805cc5602a6f1566a023fd5cf9811
James Grantham <james.r.grantham@gmail.com>
2020-03-13 08:58:44 +0000

    hash ex1

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -26,7 +26,7 @@ def proof_of_work(last_proof):
     proof = 0
     #  TODO: Your code here
 
-    while valid_proof(last_proof, proof) is False:
+    while valid_proof(last_proof, proof) is False and (timer() - start) < 20:
         proof = random.random()
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
