39f889581fb2dd324afeb423ff9931df4e350624
James Grantham <james.r.grantham@gmail.com>
2020-03-13 09:19:22 +0000

    hash ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,20 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
+    # loop over tickets
+    for ticket in tickets:
+        # insert into hash table key=S, val=D
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    key = 'NONE'
+    # loop over hash table to find next ticket
+    for i in range(0, (length)):
+        # retrieve S=none, D=val
+        current = hash_table_retrieve(hashtable, key)
+        # set S=val
+        key = current
+        # set route index
+        route[i] = key
+    return route[:-1]
+
     pass
